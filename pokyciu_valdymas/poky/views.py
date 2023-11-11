71ef83c2e4ecdfe001c0998e62875308dd67d46c
Julius Butkus <j.butkus.skouds@gmail.com>
2023-11-11 10:39:33 +0200

    index view

--- pokyciu_valdymas/poky/views.py
+++ pokyciu_valdymas/poky/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from typing import Any
+from datetime import date, timedelta
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.paginator import Paginator
+from django.db.models.query import QuerySet, Q
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from . import models
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    """
+    Renders the 'index.html' template with the given request and context.
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+    Returns:
+        HttpResponse: The rendered 'index.html' template with the given context.
+    """
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+    context = {
+        'num_visits': num_visits,
+    }
+    return render(request, 'gardenplaner/index.html', context)
